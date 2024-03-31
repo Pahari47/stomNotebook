a4e086f6a7d13c5b47ec49e336262903b5d3c622
Pahari47 <nachiketapahari00@gmail.com>
2024-03-31 11:45:40 +0530

    addnote on clientsite

--- src/context/notes/NotesState.js
+++ src/context/notes/NotesState.js
@@ -13,7 +13,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "66059d9d2eef1fd0d20ba529",
+          "_id": "66059d9d2eef1fd0d20ba527",
           "user": "660457cda84a2fafa4d39abc",
           "title": "MY Title",
           "description": "Please wake up early",
@@ -22,7 +22,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "66059d9d2eef1fd0d20ba529",
+          "_id": "66059d9d2eef1fd0d20ba528",
           "user": "660457cda84a2fafa4d39abc",
           "title": "MY Title",
           "description": "Please wake up early",
@@ -31,7 +31,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "66059d9d2eef1fd0d20ba529",
+          "_id": "66059d9d2eef1fd0d20ba526",
           "user": "660457cda84a2fafa4d39abc",
           "title": "MY Title",
           "description": "Please wake up early",
@@ -40,7 +40,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "66059d9d2eef1fd0d20ba529",
+          "_id": "66059d9d2eef1fd0d20ba525",
           "user": "660457cda84a2fafa4d39abc",
           "title": "MY Title",
           "description": "Please wake up early",
@@ -49,7 +49,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "66059d9d2eef1fd0d20ba529",
+          "_id": "66059d9d2eef1fd0d20ba524",
           "user": "660457cda84a2fafa4d39abc",
           "title": "MY Title",
           "description": "Please wake up early",
@@ -58,11 +58,35 @@ const NoteState = (props)=>{
           "__v": 0
         },
       ]
+
       const [notes, setNotes] = useState(notesInitial)
 
+      // Add a Note
+      const addNote = (title, description, tag)=>{
+        // TODO: API Call
+        console.log("Adding a new note")
+        const note = {
+            "_id": "66059d9d2eef1fd0d20ba514",
+            "user": "660457cda84a2fafa4d39abc",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "date": "2024-03-28T16:41:01.779Z",
+            "__v": 0
+          };
+        setNotes(notes.concat(note))
+      }
+      // Delete a Note
+      const deleteNote = ()=>{
+
+      }
+      // Edit a Note
+      const editNote = ()=>{
+
+      }
 
     return(
-        <NoteContext.Provider value={{notes, setNotes}}>
+        <NoteContext.Provider value={{notes, addNote, deleteNote, editNote }}>
             {props.children}
         </NoteContext.Provider>
     )
